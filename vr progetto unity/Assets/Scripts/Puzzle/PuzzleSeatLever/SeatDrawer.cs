682b3fd38e0d2625e4ca4e213b672184d8f84506
Eleonora Oberto s339013 <s339013@studenti.polito.it>
2025-12-25 00:28:16 +0100

    Sistemazione vagone cabine e vagone start (modifiche dette insieme + collider e rigid bosy + gerarchia delle scene con empty)

--- vr progetto unity/Assets/Scripts/Puzzle/PuzzleSeatLever/SeatDrawer.cs
+++ vr progetto unity/Assets/Scripts/Puzzle/PuzzleSeatLever/SeatDrawer.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class SeatDrawer : MonoBehaviour
 {
 
-    [SerializeField] private float maxOpenDistance = 0.4f;
-    [SerializeField] private float openStep = 0.05f;
-    [SerializeField] private float closeSpeed = 0.15f;
+    [SerializeField] private float maxOpenDistance = 10f;
+    [SerializeField] private float openStep = 1f;
+    [SerializeField] private float closeSpeed = 0.25f;
 
     private Vector3 closedPos;
     private float currentOpen;
@@ -34,6 +34,6 @@ public class SeatDrawer : MonoBehaviour
 
     private void UpdatePosition()
     {
-        transform.localPosition = closedPos + Vector3.forward * currentOpen;
+        transform.localPosition = closedPos - Vector3.right * currentOpen;
     }
 }
