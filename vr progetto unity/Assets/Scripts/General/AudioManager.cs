6652c5a8f406a7e27c2d1e1da79fbffa3ed42522
cegliotto <a.ceglia03@gmail.com>
2026-01-10 15:07:45 +0100

    no more bumps

--- vr progetto unity/Assets/Scripts/General/AudioManager.cs
+++ vr progetto unity/Assets/Scripts/General/AudioManager.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] AudioMixerSnapshot fullVolume;
+    [SerializeField] AudioMixerSnapshot noVolume;
+    [SerializeField] float transitionSpeed = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        fullVolume = mixer.FindSnapshot("fullVolume");
+        noVolume = mixer.FindSnapshot("noVolume");
+        noVolume.TransitionTo(0.01f); //per essere sicuri
+        fullVolume.TransitionTo(transitionSpeed);
     }
 }
