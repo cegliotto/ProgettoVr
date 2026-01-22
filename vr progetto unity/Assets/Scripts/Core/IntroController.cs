066afcb370150fd010dbe729afed506926b265a4
Eleonora Oberto s339013 <s339013@studenti.polito.it>
2026-01-22 18:14:48 +0100

    Bozza scena iniziale e finale

---
+++ vr progetto unity/Assets/Scripts/Core/IntroController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class IntroController : MonoBehaviour
+{
+    [SerializeField] private LevelLoader levelLoader;
+    [SerializeField] private GameObject startButton;
+
+    private void Start()
+    {
+        if (startButton != null)
+            startButton.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        levelLoader.LoadNextScene("trainStation");
+    }
+}
